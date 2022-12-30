261c75fde026e851239e4999f7129c486991a16e
IsaacDSC <isaac8.silva@hotmail.com>
2022-12-29 22:18:08 -0300

    feat: create middleware

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { PrismaService } from '@database/prisma.service';
 import { CatsController } from './cats.controller';
 import { CatsRepository } from './cats.repository';
 import { CatsService } from './cats.service';
 import { CatsValidation } from './cats.validate';
+import { LoggerMiddleware } from './logger.middleware';
 @Module({
+  imports: [CatsModule],
   controllers: [CatsController],
   providers: [CatsService, PrismaService, CatsRepository, CatsValidation],
 })
-export class CatsModule {}
+export class CatsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
